36a45bf4b5de582f207f37ff493355ca93199acf
mkleung-dev <mkleung@example.com>
2020-08-24 19:55:46 +0800

    Updates

---
+++ precache-manifest.91ad80599aa64b2149af0a55741cae97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0a495238947be0c1582e4ddb324391",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "294400d1cfeb30787a88",
+    "url": "/react-tictactoe/static/css/main.33764046.chunk.css"
+  },
+  {
+    "revision": "98073810baec7732683e",
+    "url": "/react-tictactoe/static/js/2.ade271f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.ade271f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294400d1cfeb30787a88",
+    "url": "/react-tictactoe/static/js/main.3d60bc22.chunk.js"
+  },
+  {
+    "revision": "db82b3e8f146d9408a77",
+    "url": "/react-tictactoe/static/js/runtime-main.c287636c.js"
+  }
+]);
